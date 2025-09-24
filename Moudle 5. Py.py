0d423baa193c6f307584222029d85cb734a05fea
neha910-star <dahalneha21@gmail.com>
2025-09-24 12:23:28 +0300

    Add files via upload

---
+++ Moudle 5. Py.py
@@ -0,0 +1,12 @@
+#list of predefined colors
+colors = ["Black", "White", "Pink", "Yellow" ,"Red" ]
+#Ask favourite color from user
+user_color = input("what is your favourite color?")
+
+#Check if color in our list
+if user_color in colors:
+    print("Excellent choice!")
+
+else:
+    print("That is a unique choice!")
+
